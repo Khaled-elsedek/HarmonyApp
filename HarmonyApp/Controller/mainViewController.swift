066c20c108d3797644793604b267f7c85ab22fe1
khaled elsedek <kh.elsedek@gmail.com>
2019-08-26 15:47:39 +0200

    for mac

--- HarmonyApp/Controller/mainViewController.swift
+++ HarmonyApp/Controller/mainViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import Cosmos
+import fluid_slider
+import SideMenu
 
 class mainViewController: UIViewController {
-
+    @IBOutlet weak var filterView: UIView!
+    
+    @IBOutlet weak var ratingCosmos: CosmosView!
+    var filterState:Bool = false
+    var filterSateCounter = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ratingCosmos.rating = 2.5
+        ratingCosmos.settings.fillMode = .precise
+        ratingCosmos.didFinishTouchingCosmos = {
+            rating in
+            self.filterSateCounter = rating
+            print(self.filterSateCounter)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func filterBtnWasBressed(_ sender: Any) {
+        filterstate()
     }
-    */
+    @IBAction func sideMenuBtnWasPressed(_ sender: Any) {
+        sideMenu()
+    }
+    @IBAction func saveFilterOptionBtnWasPressed(_ sender: Any) {
+        filterstate()
+    }
+    @IBAction func cancelFilterBtnWasPressed(_ sender: Any) {
+        filterstate()
+    }
+    func sideMenu() {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "sideMenu") else { return }
+        let menuLeftNavigationController = UISideMenuNavigationController(rootViewController: vc)
+        SideMenuManager.default.menuLeftNavigationController = menuLeftNavigationController
+        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+        SideMenuManager.default.menuFadeStatusBar = false
+        
+    }
 
+    func filterstate(){
+        if filterState == false {
+            filterState = true
+            navigationController?.navigationBar.isHidden = true
+            filterView.isHidden = false
+            
+        } else {
+            filterState = false
+            navigationController?.navigationBar.isHidden = false
+            filterView.isHidden = true
+        }
+    }
 }
